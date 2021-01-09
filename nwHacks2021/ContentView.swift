0d6cc09bb946271fdb4748b2ebfc1a626704dc70
Edward Luo <edward.luo@mail.utoronto.ca>
2021-01-09 16:11:16 -0500

    Initial Commit

---
+++ nwHacks2021/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nwHacks2021
+//
+//  Created by Edward Luo on 2021-01-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
