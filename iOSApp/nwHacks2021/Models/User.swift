cb287f6e1193e21bbb0953dd68c667a4efe665b1
Edward Luo <edward.luo@mail.utoronto.ca>
2021-01-09 22:05:41 -0500

    Add user model User profile view basic information

--- iOSApp/nwHacks2021/Models/User.swift
+++ iOSApp/nwHacks2021/Models/User.swift
@@ -5,19 +5,46 @@
 //  Created by Edward Luo on 2021-01-09.
 //
 
+import Combine
 import Foundation
 import SwiftUI
 
+// MARK: - Data types
 struct User {
-    let UserName: String
+    let userName: String
     
     var firstName: String
     var lastName: String?
     var email: String
     var photo: Image?
     
+    var bio: String?
+    var interests: [String]
+    
     var posts: [Post]
     var streaks: Int
     
     var followers: [User]
+    
+    static func defaultUser() -> User {
+        return User(userName: "TesterUserName",
+                    firstName: "Alice",
+                    lastName: "Li",
+                    email: "tester@McTest.face",
+                    photo: Image("alice"),
+                    bio: "This is her bio!",
+                    interests: ["potato"],
+                    posts: [],
+                    streaks: 10,
+                    followers: [])
+    }
+}
+
+// MARK: - Observable Data Containers
+class UserData: ObservableObject {
+    @Published var user: User
+    
+    init(user: User? = nil) {
+        self.user = user ?? User.defaultUser()
+    }
 }
--- iOSApp/nwHacks2021/Views/Profile/UserProfileView.swift
+++ iOSApp/nwHacks2021/Views/Profile/UserProfileView.swift
@@ -9,13 +9,96 @@ import Foundation
 import SwiftUI
 
 struct UserProfileView: View {
+    
+    @EnvironmentObject var userObject: UserData
+    
+    let profileImageSize: CGFloat = 80
+    let iconSize: CGFloat = 20
+    
     var body: some View {
-        Text("UserProfileView")
+        ScrollView {
+            VStack {
+                HStack(alignment: .top) {
+                    // Profile Image
+                    Group {
+                        if let safeImage = userObject.user.photo {
+                            safeImage
+                                .resizable()
+                        } else {
+                            Image(systemName: "person.circle.fill")
+                                .resizable()
+                        }
+                    }
+                    .aspectRatio(1, contentMode: .fill)
+                    .frame(width: profileImageSize)
+                    .clipShape(Circle())
+                    
+                    // Text
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text(userObject.user.userName)
+                                .font(.headline)
+                            Spacer()
+                            Button(action: {
+                                
+                            }, label: {
+                                Text("Settings")
+                                    .foregroundColor(.white)
+                                    .padding(3)
+                                    .background(Color.blue)
+                                    .cornerRadius(5)
+                            })
+                        }
+                        Text(userObject.user.firstName + " " + (userObject.user.lastName ?? ""))
+                            .font(.subheadline)
+                        if let safeBio = userObject.user.bio {
+                            Text(safeBio)
+                                .font(.caption)
+                        }
+                        
+                    }
+                }
+                HStack {
+                    Image(systemName: "flame")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: iconSize)
+                        .foregroundColor(.orange)
+                    Text("\(userObject.user.streaks)")
+                    
+                    Spacer()
+                    
+                    Image(systemName: "person.2")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: iconSize)
+                        .foregroundColor(.blue)
+                    Text("\(userObject.user.followers.count)")
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        
+                    }, label: {
+                        HStack {
+                            Image(systemName: "chart.bar.xaxis")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(height: iconSize)
+                                .foregroundColor(.blue)
+                            Text("Stats")
+                        }
+                    })
+                }
+            }
+            .padding(.horizontal)
+        }
     }
 }
 
 struct UserProfileView_Previews: PreviewProvider {
     static var previews: some View {
         UserProfileView()
+            .environmentObject(UserData())
     }
 }
